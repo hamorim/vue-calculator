2f5c5819dca1baeaa850c0b10fb3f9e75e01bd63
Hugo Lyra <me@hugolyra.com>
2020-05-31 12:41:15 -0300

    Add calculator.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="flex flex-col items-center mt-32">
+      <h1 class="text-gray-700 text-6xl font-medium tracking-tight font-sans">VCalculator</h1>
+      <p class="text-gray-500 -mt-6 pl-4">Proudly made with vue.js and tailwind.css</p>
+    </div>
+    <Calculator />
+    <div class="flex flex-col items-center -mt-24">
+      <p class="text-gray-500"><span class="text-gray-400">Tips:</span> Click on the visor to clear</p>
+      <p class="text-gray-400">Source code can be found on <a class="text-gray-500 cursor-pointer hover:text-gray-700 underline" href="https://github.com/hamorim">github</a></p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './components/Calculator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calculator
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="bg-gray-300 pb-6 w-full sm:w-2/3 lg:w-1/3 mx-auto my-32 rounded-lg overflow-hidden shadow-xl">
+    <div class="bg-gray-600 px-1 py-3 shadow-md">
+      <div @click='clear' class="flex justify-end bg-gray-400 rounded-lg text-5xl text-gray-800 font-light tracking-tight mx-10 pr-4 cursor-pointer">
+        {{ expression || 'VCal 0.1' }}
+      </div>
+    </div>
+    <div class="grid grid-cols-4 mx-10 my-4">
+      <button @click='click' class="btn">7</button>
+      <button @click='click' class="btn">8</button>
+      <button @click='click' class="btn">9</button>
+      <button @click='click' class="btn bg-orange-400">+</button>
+      <button @click='click' class="btn">4</button>
+      <button @click='click' class="btn">5</button>
+      <button @click='click' class="btn">6</button>
+      <button @click='click' class="btn bg-orange-400">-</button>
+      <button @click='click' class="btn">1</button>
+      <button @click='click' class="btn">2</button>
+      <button @click='click' class="btn">3</button>
+      <button @click='click' class="btn bg-orange-400">/</button>
+      <button @click='click' class="btn">0</button>
+      <button @click='click' class="btn">.</button>
+      <button @click='result' class="btn bg-orange-700">=</button>
+      <button @click='click' class="btn bg-orange-400">*</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      expression: ''
+    };
+  },
+  methods: {
+    click(event) {
+      this.expression += event.target.innerText;
+    },
+    result() {
+      this.expression = eval(this.expression);
+    },
+    clear() {
+      this.expression = '';
+    }
+  },
+};
+</script>
+
+<style>
+.btn {
+  @apply bg-gray-700 text-3xl text-white m-1 py-2 rounded outline-none cursor-pointer;
+}
+.btn:hover {
+  @apply bg-white text-gray-800;
+}
+.btn:focus {
+  @apply outline-none;
+}
+</style>
